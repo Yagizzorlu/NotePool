82282ffb907ebe2f07af0ef75f8768bfea7739fb
Yağız <Yaz@DESKTOP-PJG9MKS>
2025-11-19 15:29:34 +0300

    new

--- Core/NotePool.Application/ServiceRegistration.cs
+++ Core/NotePool.Application/ServiceRegistration.cs
@@ -10,6 +10,7 @@ namespace NotePool.Application
 {
     public static class ServiceRegistration
     {
+
         public static void AddApplicationServices(this IServiceCollection collection)
         {
             collection.AddMediatR(typeof(ServiceRegistration));
